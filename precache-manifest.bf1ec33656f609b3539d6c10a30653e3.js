6810d1bf00fca56fece1a8e3ec98244efea4abe4
dincengincan <engincandinc@gmail.com>
2020-06-06 18:42:23 +0300

    Updates

---
+++ precache-manifest.bf1ec33656f609b3539d6c10a30653e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e95efb24ee4fbb47ee5a2cfc72abfc10",
+    "url": "/inventory-tracking-app/index.html"
+  },
+  {
+    "revision": "bb1493b9cd3ef44db6e8",
+    "url": "/inventory-tracking-app/static/css/main.e40eb285.chunk.css"
+  },
+  {
+    "revision": "66b55a553e15a1600f70",
+    "url": "/inventory-tracking-app/static/js/2.6773b063.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/inventory-tracking-app/static/js/2.6773b063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb1493b9cd3ef44db6e8",
+    "url": "/inventory-tracking-app/static/js/main.ef89f895.chunk.js"
+  },
+  {
+    "revision": "7808bfbdc18a8236c978",
+    "url": "/inventory-tracking-app/static/js/runtime-main.ccdaaa9a.js"
+  }
+]);
